bca00507fb64206435f2ef43c1b954d37c734341
sehgalabhinav <83122406+sehgalabhinav@users.noreply.github.com>
2022-08-27 15:12:51 +0530

    mobile news items gap reduced

--- src/Newstack/DisplayArticles.js
+++ src/Newstack/DisplayArticles.js
@@ -5,7 +5,7 @@ const DiplayArticles = ({ NewsData, Loading, showArticles }) => {
   return Loading ? (
     <LoadingDots />
   ) : (
-    <section className="flex flex-col flex-wrap gap-10 items-start ">
+    <section className="flex flex-col flex-wrap gap-6 sm:gap-10 items-start ">
       {showArticles?.map((item) => {
         return <Newsitem item={item} key={item._id} />;
       })}
