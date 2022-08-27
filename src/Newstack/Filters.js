d0ea4bb2c887e240bbed3a8ab4d8331f3f46bc1e
sehgalabhinav <83122406+sehgalabhinav@users.noreply.github.com>
2022-08-27 15:09:38 +0530

    removed the bgs of filter

--- src/Newstack/Filters.js
+++ src/Newstack/Filters.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Filters = ({ setTopic, setCountry }) => {
   return (
-    <nav className="text-base sm:text-lg  flex gap-2  text-center">
+    <nav className="text-base sm:text-lg  flex gap-2  text-center ">
       <select
-        className="px-4 py-2 h-8 sm:h-10"
+        className="px-4 py-2  bg-transparent text-white border-white border cursor-pointer  [&>*]:text-black [&>*]:text-start"
         name="country"
         id="country"
         onChange={(e) => {
@@ -12,45 +12,25 @@ const Filters = ({ setTopic, setCountry }) => {
           console.log(e.target.value);
         }}
       >
-        <option className="text-start" value="IN">
-          India
-        </option>
-        <option className="text-start" value="CH">
-          China
-        </option>
-        <option className="text-start" value="US">
-          USA
-        </option>
+        <option value="IN">India</option>
+        <option value="CH">China</option>
+        <option value="US">USA</option>
       </select>
       <select
-        className="px-4 py-2 h-8 sm:h-10"
+        className="px-4 py-2  bg-transparent text-white border-white border cursor-pointer  [&>*]:text-black [&>*]:text-start"
         name="category"
         id="category"
         onChange={(e) => {
           setTopic(e.target.value);
         }}
       >
-        <option className="text-start" value="news">
-          General
-        </option>
-        <option className="text-start" value="entertainment">
-          Entertainment
-        </option>
-        <option className="text-start" value="business">
-          Business
-        </option>
-        <option className="text-start" value="politics">
-          Politics
-        </option>
-        <option className="text-start" value="science">
-          Science
-        </option>
-        <option className="text-start" value="sport">
-          Sports
-        </option>
-        <option className="text-start" value="tech">
-          Tech
-        </option>
+        <option value="news">General</option>
+        <option value="entertainment">Entertainment</option>
+        <option value="business">Business</option>
+        <option value="politics">Politics</option>
+        <option value="science">Science</option>
+        <option value="sport">Sports</option>
+        <option value="tech">Tech</option>
       </select>
     </nav>
   );
